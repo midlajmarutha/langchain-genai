650b743697b9bb3f0ddf140cbe08d5fa4e545bcb
Midlaj Marutha <midlajmarutha4@gmail.com>
2024-08-09 09:56:03 +0530

    message history state

--- chatbot.ts
+++ chatbot.ts
@@ -3,6 +3,8 @@ import {ChatPromptTemplate} from "@langchain/core/prompts"
 import dotenv from "dotenv"
 import promptsync from "prompt-sync"
 import {StringOutputParser} from "@langchain/core/output_parsers"
+import {InMemoryChatMessageHistory} from "@langchain/core/chat_history"
+import {RunnableWithMessageHistory} from "@langchain/core/runnables"
 
 dotenv.config();
 let input = promptsync({sigint:true})
@@ -17,15 +19,37 @@ let parser = new StringOutputParser();
 let systemtemplate = "You are computer science mentor who helps with the doubts of his mentees. You must help them clear their douts."
 let prompt = ChatPromptTemplate.fromMessages([
     ["system",systemtemplate],
-    ["user", "{text}"]
+    ["placeholder", "{chat_history}"],
+    ["human", "{input}"]
 ])
 
 let chain = prompt.pipe(model).pipe(parser);
+let messageHistories :Record<string, InMemoryChatMessageHistory> = {}
+const config = {
+    configurable: {
+      sessionId: "abc2",
+    },
+};
+  
+let withMessageHistory = new RunnableWithMessageHistory({
+    runnable:chain,
+    getMessageHistory: async (sessionid)=>{
+        if(messageHistories[sessionid] === undefined){
+            messageHistories[sessionid] = new InMemoryChatMessageHistory()
+        }
+        return messageHistories[sessionid];
+    },
+    inputMessagesKey:"input",
+    historyMessagesKey:"chat_history"
+})
 async function main(){
-    let text = input(">>>>>")
-    let result = await chain.invoke({text:text})
-    console.log(result)
+    let text = input(">>>")
+    let response = await withMessageHistory.invoke(
+        {input:text},
+        config
+    )
+    console.log(response)
     main()
 }
-
+console.log("ctrl+c to exit\n")
 main();
